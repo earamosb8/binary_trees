75809968d38c3d214b70ad2430c36c53e32cde25
somarae8 <1493@holbertonschool.com>
2020-07-24 02:35:46 +0000

    update header

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_depth - measures the depth of a node in a binary tree
+ * @tree: Pointer to the root node of the tree
+ * Return: if tree is NULL return 0
+ */
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t d;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+	while (tree->parent != NULL)
+	{
+		d++;
+		tree = tree->parent;
+	}
+	return (d);
+}
